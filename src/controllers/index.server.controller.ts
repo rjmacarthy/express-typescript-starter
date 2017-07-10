9f84219f7d5d2066154222d0b57221499a3d7d46
rjmacarthy <richardmacarthy@hotmail.com>
2017-07-10 09:14:27 +0100

    Revert

--- src/controllers/index.server.controller.ts
+++ src/controllers/index.server.controller.ts
@@ -1,10 +1,7 @@
 import { Request, Response } from 'express';
-import {IUser, User} from '../models/user.server.model';
 
 export default class IndexController {
     public static read(req: Request, res: Response, next: Function): void {
         res.render('index', { title: 'Express' });
-        const m = new User();
-        
     }
 }

94bd7272983b3ab814ba99e5c5cbdd6e78c3b0c8
rjmacarthy <richardmacarthy@hotmail.com>
2019-06-23 18:33:42 +0100

    lint format tests

--- spec/index.spec.ts
+++ spec/index.spec.ts
@@ -1,25 +1,25 @@
-import * as sinon from 'sinon';
-import { expect, assert } from "chai";
-import * as mocks from 'node-mocks-http';
-import { indexController } from "../src/controllers/index.server.controller";
+import * as sinon from 'sinon'
+import { expect, assert } from 'chai'
+import * as mocks from 'node-mocks-http'
+import { indexController } from '../src/controllers/index.server.controller'
 
-describe("Index Controller", function () {
-    let res: mocks.MockResponse<any>;
+describe('Index Controller', function() {
+  let res: mocks.MockResponse<any>
 
-    beforeEach(() => {
-        res = mocks.createResponse();
-    })
+  beforeEach(() => {
+    res = mocks.createResponse()
+  })
 
-    it("Can render the index page", async function () {
-        var req, spy;
-        spy = res.render = sinon.spy();
-        indexController.index(req, res, null);
-        expect(spy.calledOnce).to.equal(true);
-    });
+  it('Can render the index page', async function() {
+    let req, spy
+    spy = res.render = sinon.spy()
+    indexController.index(req, res, null)
+    expect(spy.calledOnce).to.equal(true)
+  })
 
-    it("Can get get json", () => {
-        indexController.msg(null, res);
-        var data = JSON.parse(res._getData());
-        expect(data.msg).to.equal("Hello!");
-    })
-});
+  it('Can get get json', () => {
+    indexController.msg(null, res)
+    const data = JSON.parse(res._getData())
+    expect(data.msg).to.equal('Hello!')
+  })
+})
--- spec/model.spec.ts
+++ spec/model.spec.ts
@@ -1,34 +1,34 @@
-import * as sinon from 'sinon';
-import { expect, assert } from "chai";
-import * as mocks from 'node-mocks-http';
-import { Example } from '../src/models/example.model';
-import { after } from 'mocha';
-import config from "../src/config/config";
-var mongoose = require('mongoose');
+import * as sinon from 'sinon'
+import { expect, assert } from 'chai'
+import * as mocks from 'node-mocks-http'
+import { Example } from '../src/models/example.model'
+import { after } from 'mocha'
+import config from '../src/config/config'
+// tslint:disable-next-line: no-require-imports
+const mongoose = require('mongoose')
 
-describe("Model Spec", function () {
-    let connectionString = config.mongodb;
+describe('Model Spec', function() {
+  let connectionString = config.mongodb
 
-    beforeEach(() => {
-        delete mongoose.connection.models['Example'];
-    });
+  beforeEach(() => {
+    delete mongoose.connection.models.Example
+  })
 
-    before(() => {
-        mongoose.Promise = global.Promise;
-        mongoose.connect(connectionString, {
-            useMongoClient : true
-        });
-    });
-
-    it('Can save a new model', async () => {
-        var example = new Example();
-        example.name = 'test';
-        var response = await example.save();
-        assert.isNotNull(response._id);
+  before(() => {
+    mongoose.Promise = global.Promise
+    mongoose.connect(connectionString, {
+      useMongoClient: true,
     })
+  })
 
-    after(() => {
-        Example.collection.drop();
-    })
+  it('Can save a new model', async () => {
+    const example = new Example()
+    example.name = 'test'
+    const response = await example.save()
+    assert.isNotNull(response._id)
+  })
 
-});
+  after(() => {
+    Example.collection.drop()
+  })
+})

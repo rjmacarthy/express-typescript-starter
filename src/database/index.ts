43dc6d26723407f7c7118c0f6af40dd26262fd05
rjmacarthy <richardmacarthy@protonmail.com>
2021-12-23 16:21:57 +0000

    fix build

--- src/database/index.ts
+++ src/database/index.ts
@@ -3,10 +3,7 @@ import * as mongoose from 'mongoose'
 
 export default (connectionString: string) => {
   mongoose
-    .connect(connectionString, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
+    .connect(connectionString)
     .catch(e => {
       console.log('Error connecting to mongo db', e.message)
     })

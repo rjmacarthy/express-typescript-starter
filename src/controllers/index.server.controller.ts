560c022f7b08071e14abad34b37ec1f56a426f02
mohammedabulsoud <malsoud@tailormed.co>
2022-11-22 21:48:06 +0200

    WIP

--- src/controllers/index.server.controller.ts
+++ src/controllers/index.server.controller.ts
@@ -1,8 +1,23 @@
 import { Request, Response } from 'express'
+import { Direction, Elevator } from '../models/Elevator'
+
+
 
 export default class IndexController {
-  public index(req: Request, res: Response, next: Function): void {
-    res.json({ ok: true })
+  public async index(req: Request, res: Response, next: Function): Promise<void> {
+
+    const e1 = new Elevator()
+    const e2 = new Elevator()
+
+    e1.moveToFloor(8, Direction.Up);
+    e1.moveToFloor(2, Direction.Down);
+
+    res.json({
+      ok: true, data: [
+        e1,
+        e2,
+      ]
+    })
   }
 
   public msg(req: Request, res: Response): void {
---
+++ src/models/Elevator.ts
@@ -0,0 +1,57 @@
+const VELOCITY_PER_FLOOR_SEC = 2 * 1000;
+
+export enum Status {
+  Idle = 0,
+  Moving = 1,
+  Faulty = 2,
+}
+
+export enum Direction {
+  Standby = 0,
+  Up = 1,
+  Down = 2
+}
+
+const directionMap = {
+  [Direction.Up]: "UP",
+  [Direction.Down]: "DOWN",
+}
+
+const sleep = async (ms: number): Promise<void> => await new Promise(f => setTimeout(f, ms));
+
+export class Elevator {
+  private static _id: number = 0;
+  id: number;
+  status: Status
+  direction: Direction
+  floor: number
+
+  constructor(status: Status = Status.Idle, direction: Direction = Direction.Standby, floor: number = 1) {
+    this.id = ++Elevator._id;
+    this.status = status;
+    this.direction = direction;
+    this.floor = floor;
+  }
+  public async moveToFloor(target: number, direction: Direction): Promise<void> {
+
+    while (this.status === Status.Moving) {
+      await sleep(VELOCITY_PER_FLOOR_SEC)
+    }
+
+    try {
+      console.info(`moveToFloor: Start moving to floor: ${target} Direction: ${directionMap[direction]}`);
+
+      this.status = Status.Moving;
+      this.direction = direction;
+
+      await sleep(VELOCITY_PER_FLOOR_SEC * Math.abs(target - this.floor));
+      this.floor = target;
+    } catch (error) {
+      this.status = Status.Faulty;
+      console.info(`moveToFloor: Elevator Failed to move`);
+    } finally {
+      this.status = Status.Idle;
+      console.info(`moveToFloor: Elevator finished move, status is Idle`);
+    }
+  }
+}

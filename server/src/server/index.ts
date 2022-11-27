18efe44ba8be03b93bb864ee110a082c64bedcf5
mohammedabulsoud <malsoud@tailormed.co>
2022-11-28 01:51:56 +0200

    add cors

--- server/src/server/index.ts
+++ server/src/server/index.ts
@@ -3,13 +3,14 @@ import * as express from 'express'
 import * as logger from 'morgan'
 import * as path from 'path'
 
+const cors = require('cors');
+
 import { ROUTES_DIR } from '../var/config'
 import { globFiles } from '../helpers'
 
 const app: express.Express = express()
 
-
-
+app.use(cors); /* NEW */
 app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))

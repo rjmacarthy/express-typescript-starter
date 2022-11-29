a9e36148d7b76459ac2e7f9c78dd6f52fa0d6a56
mohammedabulsoud <malsoud@tailormed.co>
2022-11-29 20:32:48 +0200

    when click on Buildings, navigate to building view page

--- client/src/Building/list.js
+++ client/src/Building/list.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { getBuildings } from "../api";
 import { Card, Col, Row, Typography } from "antd";
 import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
 import { sliceIntoChunks } from "../utils";
 import { ROUTES } from "../constant";
@@ -11,6 +12,7 @@ const { Title } = Typography;
 export const BuildingList = () => {
   const [isFetching, setIsFetching] = useState(true);
   const [buildingList, setBuildingList] = useState([]);
+  const history = useHistory();
 
   const fetchBuildings = async () => {
     setIsFetching(true);
@@ -28,6 +30,10 @@ export const BuildingList = () => {
   }, []);
 
   const buildingChunks = sliceIntoChunks(buildingList, 3);
+
+  const handleClick = (buildingId) =>
+    history.push(`${ROUTES.BULGING.list}/${buildingId}`);
+
   return (
     <>
       {isFetching ? (
@@ -39,7 +45,14 @@ export const BuildingList = () => {
               <Row key={idx} gutter={16}>
                 {group.map((building, index) => (
                   <Col key={index} span={8}>
-                    <Card title={`Building ${building.id}`} bordered={false}>
+                    <Card
+                      title={`Building ${building.id}`}
+                      bordered={false}
+                      onClick={() => handleClick(building.id)}
+                      style={{
+                        cursor: "pointer",
+                      }}
+                    >
                       Number of Floors: {building.floors}
                       <br />
                       Number of Elevators: {building.elevatorsCount}
